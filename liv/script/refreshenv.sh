e15252874132464250395b8f37d85121d785bbcf
mouad benb <mouadcom-info1@yahoo.fr>
2019-08-27 22:13:35 +0200

    retrieve DEVEF Repo: 20190827_21H42

---
+++ liv/script/refreshenv.sh
@@ -0,0 +1,20 @@
+. $HOME/.bash_profile
+export BR=$1
+export SCR=liv/script
+export TS=$(date +%Y%m%d_%HH%M)
+cd ../$BR
+git push origin $BR:$BR
+git pull origin $BR
+$SCR/switchpkgdir.sh force-app
+wk
+sfdx force:source:retrieve -x manifest/package.xml
+git add .
+git commit -m "retrieve $BR Repo: $TS"
+
+
+
+
+
+
+
+
